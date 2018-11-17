d57520b229bd12489f9e43d805ea4a6909909f34
Nick Choi <nickswchoi@gmail.com>
2018-11-16 19:33:15 -0500

    Added more input and outputs and comments

--- DinoGame.v
+++ DinoGame.v
@@ -1,5 +1,30 @@
-module DinoGame(SW, LEDR);
-	input [9:0] SW;
-	output [9:0] LEDR;
+/* Dinosaur Verilog Game */
+module DinoGame(KEY, LEDR, CLOCK_50);
+	
+	/* FPGA I/O */
+	input CLOCK_50;
+	input [2:0] KEY; // KEY[0] = JUMP, KEY[1] = PAUSE, KEY[2] = RESET.
+	output [1:0] LEDR;
+	
+	/* Wires */
+	wire clk, jump, pause, reset;
+	
+	/* Assignment Statements */
+	assign clk = CLOCK_50;
+	assign jump = KEY[0];
+	assign pause = KEY[1];
+	assign reset = KEY[2];
+	
+endmodule
+
+
+/* Datapath: Sends Signals */
+module datapath(clk, reset);
+
+endmodule
+
+
+/* Controller:  */
+module control(clk, reset);
 
 endmodule
